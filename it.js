b01d22abbff97bcd3c967b1eeb0b437665aca3da
Divin_chr <telimberedivi@gmailcom>
2025-06-23 08:42:06 +0200

    separating js

--- it.js
+++ it.js
@@ -1,18 +1,30 @@
-function productArray(numbers){
-  //your code here
-  
-let prod = [];
-  let product = 1;
-  
-  for (let i=0; i<= numbers.length; i++){
-    product = product*numbers[i];
-  
-  }
-  
-  for (let i=0; i<= numbers.length; i++){
-      prod = prod + product/numbers[i];
-  }
-  
-  return prod;
-  
-}
+ let keyToAudio = {
+        W: "1",
+        A: "2",
+        S: "3",
+        D: "4",
+        J: "5",
+        K: "6",
+        L: "7",
+      };
+
+      Object.keys(keyToAudio).forEach((key) => {
+        const btn = document.getElementById(key);
+        const audioId = keyToAudio[key];
+        const audio = document.getElementById(audioId);
+
+        btn.addEventListener("click", () => {
+          audio.currentTime = 0; // rewind to start
+          audio.play();
+        });
+      });
+
+      document.addEventListener("keydown", (event) => {
+        const key = event.key.toUpperCase();
+        if (keyToAudio[key]) {
+          const audioId = keyToAudio[key];
+          const audio = document.getElementById(audioId);
+          audio.currentTime = 0; // rewind to start
+          audio.play();
+        }
+      });
